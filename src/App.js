a3787a70c7f2e51a4c9b49c1dc11c6e23a377a54
Cicero Raldi da Fonseca <cfonseca@northern.co>
2021-08-01 09:50:28 -0400

    Create react app and install semantic ui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+      <header>
+        <h2 className='main-header'>React CRUD Operations</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
